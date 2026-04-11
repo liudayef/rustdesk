391b4f6c8cceca188e48e0a6a18930128e31f826
liudayef <568233278@qq.com>
2026-04-11 19:06:18 +0800

    注释掉1660-1666服务器设置入口

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1657,13 +1657,13 @@ class _NetworkState extends State<_Network> with AutomaticKeepAliveClientMixin {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (!hideServer)
-                listTile(
-                  icon: Icons.dns_outlined,
-                  title: 'ID/Relay Server',
-                  onTap: () => showServerSettings(gFFI.dialogManager, setState),
-                ),
-              if (!hideProxy && !hideServer) divider,
+            //  if (!hideServer)
+            //    listTile(
+            //      icon: Icons.dns_outlined,
+            //      title: 'ID/Relay Server',
+            //      onTap: () => showServerSettings(gFFI.dialogManager, setState),
+            //    ),
+            //  if (!hideProxy && !hideServer) divider,
               if (!hideProxy)
                 listTile(
                   icon: Icons.network_ping_outlined,
